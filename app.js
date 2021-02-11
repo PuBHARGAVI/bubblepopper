74f699dd09c231bba0f2a8cd359529b5f85f2e89
PuBHARGAVI <saibhargavi.puvvada@gmail.com>
2021-02-11 15:52:23 +0530

    1499215

--- app.js
+++ app.js
@@ -1,108 +1,167 @@
-var arr=['bhargavi','akhila','srimathi','rhea','harsh'];
-var sc=0;
+var words=['bhargavi','akhila','srimathi','rhea','harsh'];
+var score=0;
 var s="";
 var c=0;
 var hitc=0;
 var arrele=[];
 var level=1;
+var leveltimer=3;
 var levelarray=["level0","level1","level2","level3"];
 function start(){
+    var timeremaining=15000;
     document.getElementById("div0").style.display='none';
     document.getElementById("div1").style.display='block';
-    document.getElementById("text").innerHTML=arr[0];
+    document.getElementById("text").innerHTML=words[0];
     var interval=setInterval(function(){
-            console.log(arr[c]);
+        if(level==4){
+            clearInterval(interval);
+        }    
+        console.log(words[c]);
             console.log(s);
             //ReactDOM.render(ele,document.getElementById('root'));
             if(level==4){
-                document.getElementById("div1").style.display='none';
-                document.getElementById("over").innerHTML=sc;
-                document.getElementById("gameover").style.display='block';
-                clearInterval(interval);
+                    clearInterval(interval);
             }
-            if(c==arr.length-1){
+            if(c==words.length-1){
                 if(level==1){
-                    if(arr[c]==s){
-                        sc=sc+1;
+                    if(words[c]==s){
+                        score=score+1;
                     }
-                if(sc>=4){
-                    console.log("ji");
+                if(score>=4){
                     level=level+1;
                     console.log(level);
                     c=-1;
-                    document.getElementById("score").innerHTML=sc;
-                document.getElementById("level").innerHTML=levelarray[level];
+                    words=['veda','preethi','manju','venkatesh','vyshnavi'];
+                    document.getElementById("score").innerHTML=score;
+                    document.getElementById("level").innerHTML=levelarray[level];
                 }
                 else{
-                    document.getElementById("over").innerHTML=sc;
+                    document.getElementById("over").innerHTML=score;
+                    document.getElementById("div1").style.display='none';
+                document.getElementById("lost").style.display='block';
                 document.getElementById("gameover").style.display='block';
                 clearInterval(interval);
                 }
                 }
             else if(level==2){
-                if(arr[c]==s){
-                    sc=sc+2;
+                if(words[c]==s){
+                    score=score+2;
                 }
-                if(sc>=9){
+                if(score>=9){
                     console.log("level2");
                     level=level+1;
                     c=-1;
-                    document.getElementById("score").innerHTML=sc;
+                    words=['gayathri','manju','atchaya','veda','rani'];
+                    document.getElementById("score").innerHTML=score;
                 document.getElementById("level").innerHTML=levelarray[level];
-                }
+            }
+
                 else{
-                    document.getElementById("over").innerHTML=sc;
+                    document.getElementById("over").innerHTML=score;
+                    document.getElementById("div1").style.display='none';
+                document.getElementById("lost").style.display='block';
                 document.getElementById("gameover").style.display='block';
                 clearInterval(interval);
                 }
             }
             else if(level==3){
-                if(arr[c]==s){
-                    sc=sc+3;
+                if(words[c]==s){
+                    score=score+3;
                 }
-                if(sc>=18){
+                if(score>=18){
                     level=level+1;
                     c=-1;
-                    document.getElementById("score").innerHTML=sc;
+                    document.getElementById("score").innerHTML=score;              
                 document.getElementById("level").innerHTML=levelarray[level];
-                }
+            }
                 else{
-                    document.getElementById("over").innerHTML=sc;
+                    document.getElementById("over").innerHTML=score;
+                    document.getElementById("div1").style.display='none';
+                document.getElementById("lost").style.display='block';
                 document.getElementById("gameover").style.display='block';
                 clearInterval(interval);
                 }
             }
             }
-            else if(arr[c]==s){
-                if(level==1){
-                sc=sc+1;
-                }
-                else if(level==2){
-                sc=sc+2;
-                }
-                else if(level==3){
-                sc=sc+3;
-                }
-                document.getElementById("score").innerHTML=sc;
+            else if(words[c]==s){
+                scoreincrement();
+                document.getElementById("score").innerHTML=score;
                 console.log(c);
             }
-                console.log("bow");
-                console.log(c);
-        var audio = new Audio('assets/nextbutton.mp3');
-        audio.play();
+            console.log(c);
+            nextbuttonaudio();
         s="";
-        c=c+1;
-        document.getElementById("text").innerHTML=arr[c];
-        for(var i=0;i<arrele.length;i++){
-            document.getElementById(arrele[i]).style.display='block';
+        if(c==-1){
+            wait();
+            console.log("level");
+            console.log(level);
+            timeremaining=timeremaining+5000
         }
-    }, 15000);
+        c=c+1;
+        console.log("after");
+        console.log(c);
+        console.log
+        console.log(words);
+        document.getElementById("text").innerHTML=words[c];
+        showbubble();
+        
+    }, timeremaining);
     }
-function hide(id){
+function scoreincrement(){
+    if(level==1){
+        score=score+1;
+        }
+        else if(level==2){
+        score=score+2;
+        }
+        else if(level==3){
+        score=score+3;
+        }
+}
+function nextbuttonaudio(){
+    var audio = new Audio('assets/nextbutton.mp3');
+    audio.play();
+}    
+function playbubbleaudio(){
     var audio = new Audio('assets/bubblesound.mp3');
     audio.play();
+}    
+function hidebubble(id){
+    console.log(document.getElementById(id).value);
+    playbubbleaudio();
     s=s+document.getElementById(id).value;
     document.getElementById(id).style.display = 'none';
     hitc=hitc+1;
     arrele.push(id);
+}
+function showbubble(){
+    for(var i=0;i<arrele.length;i++){
+        document.getElementById(arrele[i]).style.display='block';
+    }
+}
+function editcharacter(){
+    var popele=arrele.pop();
+    s = s.substring(0, s.length-1);
+    document.getElementById(popele).style.display='block';
+}
+function wait(){
+    document.getElementById("div1").style.display='none';
+    if(level==4){
+        document.getElementById("div1").style.display='none';
+        document.getElementById("over").innerHTML=score;
+        document.getElementById("gameover").style.display='block';
+    }
+if(level!=4){
+    var levelinterval=setInterval(function(){
+        document.getElementById("levelinterval").style.display='block';   
+        document.getElementById("timer").innerHTML=leveltimer;
+           leveltimer=leveltimer-1;
+           if(leveltimer==0){
+               leveltimer=5;
+               document.getElementById("div1").style.display='block';
+               document.getElementById("levelinterval").style.display='none';
+               clearInterval(levelinterval);
+           } 
+    },1000);
+}
 }
